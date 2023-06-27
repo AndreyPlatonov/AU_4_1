a9267cc543a47fdf0bf785d8e49b9f40bff7a845
AndreyPlatonov <one.and.pl@gmail.com>
2023-06-27 13:11:41 +0300

    dz_4_1 fix6

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -25,20 +25,18 @@ public class CallbackTest {
     void submitSuccessRequest() {
 
         FormatDate dateFormatter = new FormatDate();
+        String dateMeeting = dateFormatter.currentPlusDays(4);
+
         $("[data-test-id=city] input").setValue("Томск");
-        $("[data-test-id=date] input").clear();
-        $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys(" ");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.DELETE);
-        $("[data-test-id=date] input").setValue(dateFormatter.currentPlusDays(4));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(dateMeeting);
         $("[data-test-id=name] input").setValue("Иванов Владимир");
         $("[data-test-id=phone] input").setValue("+79201234567");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        //$(".button_view_extra").click();
-        $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
-
+        // $(".button_view_extra").click();
+        // $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + dateMeeting), Duration.ofSeconds(15)).shouldBe(Condition.visible);
 
     }
 
@@ -46,18 +44,16 @@ public class CallbackTest {
     void submitFailedNullCity() {
 
         FormatDate dateFormatter = new FormatDate();
+        String dateMeeting = dateFormatter.currentPlusDays(4);
+
         $("[data-test-id=city] input").setValue("");
-        $("[data-test-id=date] input").clear();
-        $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys(" ");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.DELETE);
-        $("[data-test-id=date] input").setValue(dateFormatter.currentPlusDays(4));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(dateMeeting);
         $("[data-test-id=name] input").setValue("Иванов Владимир");
         $("[data-test-id=phone] input").setValue("+79201234567");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=city]").shouldHave(Condition.cssClass("input_invalid"));
+        $("[data-test-id=city].input_invalid .input__sub").shouldHave(Condition.text("Поле обязательно для заполнения")).shouldBe(Condition.visible);
 
     }
 
@@ -65,18 +61,16 @@ public class CallbackTest {
     void submitFailedWrongCity() {
 
         FormatDate dateFormatter = new FormatDate();
+        String dateMeeting = dateFormatter.currentPlusDays(4);
+
         $("[data-test-id=city] input").setValue("ввввввв");
-        $("[data-test-id=date] input").clear();
-        $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys(" ");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.DELETE);
-        $("[data-test-id=date] input").setValue(dateFormatter.currentPlusDays(4));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(dateMeeting);
         $("[data-test-id=name] input").setValue("Иванов Владимир");
         $("[data-test-id=phone] input").setValue("+79201234567");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=city].input_invalid .input__sub").shouldHave(Condition.exactText("Доставка в выбранный город недоступна"));
+        $("[data-test-id=city].input_invalid .input__sub").shouldHave(Condition.exactText("Доставка в выбранный город недоступна")).shouldBe(Condition.visible);
 
     }
 
@@ -84,17 +78,13 @@ public class CallbackTest {
     void submitFailedNullDate() {
 
         $("[data-test-id=city] input").setValue("Томск");
-        $("[data-test-id=date] input").clear();
-        $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys(" ");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue("");
         $("[data-test-id=name] input").setValue("Иванов Владимир");
         $("[data-test-id=phone] input").setValue("+79201234567");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=date] .input").shouldHave(Condition.cssClass("input_invalid"));
+        $("[data-test-id=date] .input.input_invalid .input__sub").shouldHave(Condition.exactText("Неверно введена дата")).shouldBe(Condition.visible);
 
 
     }
@@ -103,18 +93,16 @@ public class CallbackTest {
     void submitSuccessBoundDate() {
 
         FormatDate dateFormatter = new FormatDate();
+        String dateMeeting = dateFormatter.currentPlusDays(3);
+
         $("[data-test-id=city] input").setValue("Томск");
-        $("[data-test-id=date] input").clear();
-        $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys(" ");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.DELETE);
-        $("[data-test-id=date] input").setValue(dateFormatter.currentPlusDays(3));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(dateMeeting);
         $("[data-test-id=name] input").setValue("Иванов Владимир");
         $("[data-test-id=phone] input").setValue("+79201234567");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + dateMeeting), Duration.ofSeconds(15)).shouldBe(Condition.visible);
 
     }
 
@@ -122,37 +110,33 @@ public class CallbackTest {
     void submitFailedBeforeDate() {
 
         FormatDate dateFormatter = new FormatDate();
+        String dateMeeting = dateFormatter.currentPlusDays(1);
+
         $("[data-test-id=city] input").setValue("Томск");
-        $("[data-test-id=date] input").clear();
-        $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys(" ");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.DELETE);
-        $("[data-test-id=date] input").setValue(dateFormatter.currentPlusDays(1));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(dateMeeting);
         $("[data-test-id=name] input").setValue("Иванов Владимир");
         $("[data-test-id=phone] input").setValue("+79201234567");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=date] .input").shouldHave(Condition.cssClass("input_invalid"));
-        
+        $("[data-test-id=date] .input.input_invalid .input__sub").shouldHave(Condition.exactText("Заказ на выбранную дату невозможен")).shouldBe(Condition.visible);
+
     }
 
     @Test
     void submitFailedNullName() {
 
         FormatDate dateFormatter = new FormatDate();
+        String dateMeeting = dateFormatter.currentPlusDays(4);
+
         $("[data-test-id=city] input").setValue("Томск");
-        $("[data-test-id=date] input").clear();
-        $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys(" ");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.DELETE);
-        $("[data-test-id=date] input").setValue(dateFormatter.currentPlusDays(4));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(dateMeeting);
         $("[data-test-id=name] input").setValue("");
         $("[data-test-id=phone] input").setValue("+79201234567");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=name].input .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=name].input_invalid.input .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения")).shouldBe(Condition.visible);
 
     }
 
@@ -160,18 +144,16 @@ public class CallbackTest {
     void submitFailedWrongName() {
 
         FormatDate dateFormatter = new FormatDate();
+        String dateMeeting = dateFormatter.currentPlusDays(4);
+
         $("[data-test-id=city] input").setValue("Томск");
-        $("[data-test-id=date] input").clear();
-        $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys(" ");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.DELETE);
-        $("[data-test-id=date] input").setValue(dateFormatter.currentPlusDays(4));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(dateMeeting);
         $("[data-test-id=name] input").setValue("Иванов Иван222");
         $("[data-test-id=phone] input").setValue("+79201234567");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=name].input .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input.input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.")).shouldBe(Condition.visible);
 
     }
 
@@ -179,18 +161,16 @@ public class CallbackTest {
     void submitFailedNullPhone() {
 
         FormatDate dateFormatter = new FormatDate();
+        String dateMeeting = dateFormatter.currentPlusDays(4);
+
         $("[data-test-id=city] input").setValue("Томск");
-        $("[data-test-id=date] input").clear();
-        $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys(" ");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.DELETE);
-        $("[data-test-id=date] input").setValue(dateFormatter.currentPlusDays(4));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(dateMeeting);
         $("[data-test-id=name] input").setValue("Иванов Владимир");
         $("[data-test-id=phone] input").setValue("");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=phone].input .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=phone].input.input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения")).shouldBe(Condition.visible);
 
     }
 
@@ -198,18 +178,16 @@ public class CallbackTest {
     void submitFailedWrongPhone() {
 
         FormatDate dateFormatter = new FormatDate();
+        String dateMeeting = dateFormatter.currentPlusDays(4);
+
         $("[data-test-id=city] input").setValue("Томск");
-        $("[data-test-id=date] input").clear();
-        $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys(" ");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.DELETE);
-        $("[data-test-id=date] input").setValue(dateFormatter.currentPlusDays(4));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(dateMeeting);
         $("[data-test-id=name] input").setValue("Иванов Владимир");
         $("[data-test-id=phone] input").setValue("+7920123456723");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=phone].input .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input.input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.")).shouldBe(Condition.visible);
 
     }
 
@@ -217,13 +195,11 @@ public class CallbackTest {
     void submitFailedNotCheck() {
 
         FormatDate dateFormatter = new FormatDate();
+        String dateMeeting = dateFormatter.currentPlusDays(4);
+
         $("[data-test-id=city] input").setValue("Томск");
-        $("[data-test-id=date] input").clear();
-        $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys(" ");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id=date] .input__control").sendKeys(Keys.DELETE);
-        $("[data-test-id=date] input").setValue(dateFormatter.currentPlusDays(4));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(dateMeeting);
         $("[data-test-id=name] input").setValue("Иванов Владимир");
         $("[data-test-id=phone] input").setValue("+79201234567");
         $$("button").find(exactText("Забронировать")).click();
